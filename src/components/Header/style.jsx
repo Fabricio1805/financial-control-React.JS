dbb8f81f60f17cc7a14f77cfb60ad3486508125c
fabricio1805 <fabricio.ss09@outlook.com>
2022-07-29 10:51:32 -0300

    start

---
+++ src/components/Header/style.jsx
@@ -0,0 +1,16 @@
+import styled  from "styled-components";
+
+export const Container = styled.div`
+    height: 150px; 
+    text-align: center;
+    background: #121214;
+`;
+
+export const Header = styled.h1`
+    
+`;
+
+export const Title = styled.div`
+    padding-top: 15px;
+    color: #FFF; 
+`;
